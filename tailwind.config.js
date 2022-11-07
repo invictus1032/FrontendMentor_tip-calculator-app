cd7e8c0c5e457044fa4d919822bbf23d7c93819f
invictus1032 <invictus1032@gmail.com>
2022-11-07 08:44:10 +0700

    chore: add script.ts into tailwind content files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "script.js"],
+  content: ["./index.html", "script.js", "script.ts"],
   theme: {
     fontFamily: {
       "SpaceMono":["Space Mono"]
